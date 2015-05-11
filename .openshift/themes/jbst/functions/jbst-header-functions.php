8f6e8b8bd9a3b8ce14a8f834957be9c2530cbfd6
Haribabu Thilakar <haribabut@yahoo.com>
2015-05-11 12:38:28 +0530

    Added Google tag manager

--- .openshift/themes/jbst/functions/jbst-header-functions.php
+++ .openshift/themes/jbst/functions/jbst-header-functions.php
@@ -112,6 +112,22 @@ add_action( 'jbst_header', 'jbst_body_open', 40 );
 function jbst_body_open() {
 ?>
 <body <?php body_class(); ?>>
+/* Google tag manager */
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-WPGMTM" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>
+  (function(w,d,s,l,i){
+  	w[l]=w[l]||[];w[l].push({
+  		'gtm.start': new Date().getTime(),
+  		event:'gtm.js'
+  	});
+  	var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+  	j.async=true;
+  	j.src='//www.googletagmanager.com/gtm.js?id='+i+dl;
+  	f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WPGMTM');</script>
+<!-- End Google Tag Manager -->
+
 <?php do_action( 'jbst_before' ); ?>
 <div class="jbst-site-wrap">
 
